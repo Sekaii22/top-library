2074d5939df3ce3a449d46d7c84ee2037b9fd554
Sekaii22 <shawn.ng22@gmail.com>
2025-05-09 23:38:52 +0800

    Implement book adding feature

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mylibrary = [];
+const libraryContainer = document.querySelector("#library-container");
 
-function Book(author, title, pages, status) {
+function Book(title, author, pages, status) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
@@ -8,63 +9,100 @@ function Book(author, title, pages, status) {
     this.status = status;
 }
 
-function addBookToLibrary(author, title, pages, status, libraryArr) {
-    let book = new Book(author, title, pages, status);
+function addBookToLibrary(title, author, pages, status, libraryArr) {
+    let book = new Book(title, author, pages, status);
 
     if (libraryArr) {
         libraryArr.push(book);
     }
+
+    return book
 }
 
-const libraryContainer = document.querySelector("#library-container");
-const addBtn = document.querySelector("#add-btn");
-const modal = document.querySelector("#add-book-modal");
+function displayBook(book) {
+    // create nodes
+    let divCard = document.createElement("div");
+    let divBookTop = document.createElement("div");
+    let divBookBottom = document.createElement("div");
+    let pTitle = document.createElement("p");
+    let pAuthor = document.createElement("p");
+    let pPages = document.createElement("p");
+    let pHasRead = document.createElement("p");
+
+    // add to DOM
+    libraryContainer.appendChild(divCard);
+    divCard.appendChild(divBookTop);
+    divCard.appendChild(divBookBottom);
+    divBookTop.appendChild(pTitle);
+    divBookTop.appendChild(pAuthor);
+    divBookBottom.appendChild(pPages);
+    divBookBottom.appendChild(pHasRead);
+    
+    // add classes
+    divCard.classList.add("book");
+    divBookTop.classList.add("book-top");
+    divBookBottom.classList.add("book-bottom");
+    pTitle.classList.add("title");
+    pAuthor.classList.add("author");
+    pPages.classList.add("pages");
+    pHasRead.classList.add("read-status");
+
+    // add text content
+    pTitle.textContent = book.title;
+    pAuthor.textContent = "By " + book.author;
+    pPages.textContent = "Pages: " + book.pages;
+    pHasRead.textContent = "Status: " + book.status;
+}
 
 function displayLibrary(libraryArr) {
-    // loop through library arr
     // for each book, create a card in library container
     for (let i = 0; i < libraryArr.length; i++) {
-        // create nodes
-        let divCard = document.createElement("div");
-        let divBookTop = document.createElement("div");
-        let divBookBottom = document.createElement("div");
-        let pTitle = document.createElement("p");
-        let pAuthor = document.createElement("p");
-        let pPages = document.createElement("p");
-        let pHasRead = document.createElement("p");
-
-        // add to DOM
-        divBookTop.appendChild(pTitle);
-        divBookTop.appendChild(pAuthor);
-        divBookBottom.appendChild(pPages);
-        divBookBottom.appendChild(pHasRead);
-        divCard.appendChild(divBookTop);
-        divCard.appendChild(divBookBottom);
-        libraryContainer.appendChild(divCard);
-        
-        // add classes
-        divCard.classList.add("book");
-        divBookTop.classList.add("book-top");
-        divBookBottom.classList.add("book-bottom");
-        pTitle.classList.add("title");
-        pAuthor.classList.add("author");
-        pPages.classList.add("pages");
-        pHasRead.classList.add("read-status");
-
-        // add text content
-        pTitle.textContent = libraryArr[i].title;
-        pAuthor.textContent = libraryArr[i].author;
-        pPages.textContent = "Pages: " + libraryArr[i].pages;
-        pHasRead.textContent = "Status: " + libraryArr[i].status;
+        displayBook(libraryArr[i]);
     }
 }
 
-addBtn.addEventListener("click", () => {
-    modal.showModal();
+// modal nodes
+const openModalBtn = document.querySelector("#add-btn");
+const closeModalBtn = document.querySelector("#close-modal-btn");
+const modal = document.querySelector("#add-book-modal");
+const modalArea = document.querySelector("#dialog-area");
+const formAddBtn = document.querySelector("#form-add-btn");
+
+// form inputs nodes
+const form = document.querySelector("#add-book-form");
+const titleInput = document.querySelector("#title-input");
+const authorInput = document.querySelector("#author-input");
+const pagesInput = document.querySelector("#pages-input");
+const statusInput = document.querySelector("#read-status-input");
+
+openModalBtn.addEventListener("click", () => modal.showModal());
+
+closeModalBtn.addEventListener("click", () => modal.close());
+
+// clicking outside modal should closes it as well
+modal.addEventListener("click", () => modal.close());
+modalArea.addEventListener("click", (e) => {
+    e.stopPropagation();
 });
 
-addBookToLibrary("js1", "t1", 50, "Unread", mylibrary);
-addBookToLibrary("js2", "t2", 50, "Unread", mylibrary);
-addBookToLibrary("js3", "t3", 50, "Finished", mylibrary);
-addBookToLibrary("js4", "t4", 50, "Unread", mylibrary);
+formAddBtn.addEventListener("click", (e) => {
+    if (form.checkValidity()) {
+        e.preventDefault();
+    
+        let title = titleInput.value;
+        let author = authorInput.value;
+        let pages = pagesInput.value;
+        let status = statusInput.value;
+    
+        let book = addBookToLibrary(title, author, pages, status);
+        displayBook(book);
+        form.reset();
+        modal.close();
+    }
+});
+
+addBookToLibrary("t1", "js1", 50, "Unread", mylibrary);
+addBookToLibrary("t2", "js2", 50, "Unread", mylibrary);
+addBookToLibrary("t3", "js3", 50, "Finished", mylibrary);
+addBookToLibrary("t4", "js4", 50, "Unread", mylibrary);
 displayLibrary(mylibrary);
